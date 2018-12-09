0e16153292316426fb3420924e9b5bac74a2518a
Shay Pasvolsky <spasvolski@gmail.com>
2018-12-10 00:12:26 +0200

    Continued with over files
    Signed-off-by: Shay Pasvolsky <spasvolski@gmail.com>

--- globals.py
+++ globals.py
@@ -1,3 +1,33 @@
+##########################################################
+# This file is used to store all sorts of configurations #
+# Used in:                                               #
+#       main.py                                          #
+#       wishJsonMgr.py                                   #
+#       RssMonitor.py                                    #
+##########################################################
+
+#####################################################################
+# Used for global settings                                          #
+# Globals are used in:                                              #
+#   main.py:                                                        #
+#           main()                                                  #
+#           load_config()                                           #
+#           init_logger()                                           #
+#                                                                   #
+# moridim_main is used for definition of the site uri, Used in:     #
+#           moridim_rss in this file only                           #
+#      ** ??? should be moved to the ini config file ???            #
+#                                                                   #
+# moridim_rss is the full link to the rss feed to follow, Used in:  #
+#   main.py:                                                        #
+#           main()                                                  #
+#      ** ??? should be moved to the ini config file ???            #
+#                                                                   #
+# log_filename is used to define the log file name, Used in:        #
+#   main.py:                                                        #
+#           init_logger()                                           #
+#####################################################################
+
 
 class Globals:
     moridim_main = "https://www.moridim.biz"
@@ -5,15 +35,35 @@ class Globals:
     log_filename = "mDownloader.log"
 
 
-class Conf_ini:
-    conf = None
-    conf_filename = "mDownloader.ini"
+#####################################################################
+# Used for configuration manager parameters                         #
+# Used in:                                                          #
+#   main.py:                                                        #
+#           main()                                                  #
+#           load_config()                                           #
+#           init_logger()                                           #
+#   RssMonitor.py:                                                  #
+#           RssMonitor class, ??? for a print statment only ???     #
+#                                                                   #
+# Conf is initialized as empty and then used in:                    #
+#   main.py:                                                        #
+#           load_config()                                           #
+#   RssMonitor.py:                                                  #
+#           RssMonitor class, ??? for a print statement only ???    #
+#                                                                   #
+# To be continued.............
+#####################################################################
 
-    class Keys:
-        general = "General"
-        loggingLevel = "loggingLevel"
-        rss = "RSS"
-        lastUpdateTime = "lastUpdateTime"
+
+class Conf_ini:
+    conf = None # Initialize tje configuration with blank
+    conf_filename = "mDownloader.ini" # Used for the configuration ini file link
+
+    class Keys: # Used to set the name of the configuration sections and keys
+        general = "General" # Used for the general section
+        loggingLevel = "loggingLevel" # Used for the logging level parameter
+        rss = "RSS" # Used for the RSS section
+        lastUpdateTime = "lastUpdateTime" #
         checkInterval = "checkInterval"
 
 
--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
         logger.debug("logger initialized") # Logging a message ("logger initialized") in the log file with level "debug"
         Wish_json.wishJsonMgr = WishJsonMgr() # Create an instance of wishjsonmgr into Wish_Json.wishJsonMgr
         logger.debug("wish json loaded") # Logging a message ("wish json loaded") in the log file with level "debug"
-    except Exception as e: # Handle all exceptions and pass them as e (??? which is never used, why ???)
+    except Exception as e: # Handle all exceptions and pass them as e (Will be used at later time to handle exceptions)
         #logger.error(str(e)) # (??? is it needed or not ???)
         sys.exit(errno.EPERM) # Exit the program with the error "operation not permitted" (=="EPREM")
 
@@ -42,10 +42,10 @@ def main():
     # Create an instance of the RSSMonitor with the link of moridim_rss from the globals file
     monitor = RssMonitor(Globals.moridim_rss)
     # Make repeating calls for the rss monitor while passing the following parameters from the globals file:
-    # Conf_ini.Keys.rss == the RSS feed name (??? Why is it under the convertion to float ???)
-    # Conf_ini.Keys.checkInterval == the time between calls of the function
-    # monitor.monitor is the function to run under the instance of rss monitor created earlier
-    # Pass the all thing to "monitor_stop_func" instance (??? for what ???)
+    #   Conf_ini.Keys.rss == the RSS feed name
+    #   Conf_ini.Keys.checkInterval == the time between calls of the function
+    #   monitor.monitor is the function to run under the instance of rss monitor created earlier
+    # Pass the all thing to "monitor_stop_func" instance (for later use)
     monitor_stop_func = call_repeatedly(float(Conf_ini.conf.get(Conf_ini.Keys.rss, Conf_ini.Keys.checkInterval)),
                                         monitor.monitor)
     monitor.monitor # (??? why use this call, for what ???
@@ -66,11 +66,11 @@ def load_config():
     Conf_ini.conf = configparser.ConfigParser()
     # Check the configuration by reading it and insert the result to read_ok
     read_ok = Conf_ini.conf.read(Conf_ini.conf_filename)
-    # if the config file read didnt work then print "'config file name' wasn't fond"
+    # if the config file read didn't work then print "'config file name' wasn't fond"
     # Needs to throw exceprion and write to the log file
     if not read_ok:
         #TODO : throw exception
-        print(Globals.conf_filename + " wasn't found")
+        print(Globals.conf_filename + " wasn't found") # ** The Conf_filename is Under Conf_ini, Need to be fixed
 
 
 
@@ -98,9 +98,9 @@ def init_logger():
 
 
 
-###################################################################
-# Check if the file name is main, and if so run the main function #
-###################################################################
+#################################################################################
+# Check if the file is the main of the program, and if so run the main function #
+#################################################################################
 
 if __name__ == '__main__':
     main()
